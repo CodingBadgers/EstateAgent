f844af59970054d5129105139e8fdfbc4427b43e
samoatesgames <sam@samoatesgames.com>
2013-10-30 18:20:02 +0000

    Added main plugin class

---
+++ src/main/java/uk/thecodingbadgers/estateagent/EstateAgent.java
@@ -0,0 +1,13 @@
+package uk.thecodingbadgers.estateagent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author TheCodingBadgers
+ *
+ * Main entry class for the Estate Agent plugin
+ *
+ */
+public final class EstateAgent extends JavaPlugin {
+ 
+}
